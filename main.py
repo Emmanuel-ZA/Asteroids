d96244810c0837d9956fba8ad96273f1bcc0e6cc
Emmanuel-ZA <ezd36921@gmail.com>
2025-01-14 12:29:17 -0700

    Add player ship rendering

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    p1 = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while(True):
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
-                return
-        pygame.Surface.fill(screen,(0,0,0))
+                return     
+        screen.fill((0,0,0))
+        p1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    #print("Starting asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
